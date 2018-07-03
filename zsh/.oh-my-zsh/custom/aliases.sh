a92650e97bf4ae44d05c1260ccc6a34b0fa81d7c
Jann Kleen <jann.kleen@gmail.com>
2018-07-03 23:40:30 +0200

    add brew aliases that used to be in zsh plugin

--- zsh/.oh-my-zsh/custom/aliases.sh
+++ zsh/.oh-my-zsh/custom/aliases.sh
@@ -3,3 +3,6 @@
 alias vgh="vagrant halt && vagrant halt \`vagrant global-status | grep default | grep -E \$(pwd)'\s*$' | awk '{print \$1}'\`"
 alias cup="brew cask list | xargs brew cask install --force"
 alias snake="VERSIONER_PYTHON_PREFER_32_BIT=yes pythonw \`which runsnake32\`"
+alias bubo='brew update && brew outdated'
+alias bubc='brew upgrade && brew cleanup'
+alias bubu='bubo && bubc'
